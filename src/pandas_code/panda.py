27bc6263978450912b06ff9d143b0b9779034ad5
David <joshalabermu@gmail.com>
2024-07-29 18:13:47 -0600

    inserscion de laboratorio jupyter

--- src/pandas_code/panda.py
+++ src/pandas_code/panda.py
@@ -1,3 +1,17 @@
-import pandas  as pd
+import pandas as pd
+from tabulate import tabulate
 
-print(pd.__version__) 
+# Datos del medallero de los Juegos Olímpicos de Río de Janeiro 2016
+data = {
+    'País': ['Estados Unidos', 'Reino Unido', 'China', 'Rusia', 'Alemania', 'Japón', 'Francia', 'Corea del Sur', 'Italia', 'Australia'],
+    'Oro': [46, 27, 26, 19, 17, 12, 10, 9, 8, 8],
+    'Plata': [37, 23, 18, 17, 10, 8, 18, 3, 12, 11],
+    'Bronce': [38, 17, 26, 20, 15, 21, 14, 9, 8, 10],
+    'Total': [121, 67, 70, 56, 42, 41, 42, 21, 28, 29]
+}
+
+# Crear el DataFrame
+medallero_df = pd.DataFrame(data)
+
+# Mostrar el DataFrame usando tabulate
+print(tabulate(medallero_df, headers='keys', tablefmt='grid'))
